4afcac7818eb39ec44ccf696df89ab88194309a0
tiagoduartedf <tiagoduartedf@gmail.com>
2024-12-21 00:50:42 -0300

    fix pdf in prod

---
+++ _next/static/Un7rSyT9rPN1eN-mK0IGQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/348-2fcc6837688bc743.js","static/chunks/pages/index-cd80d9d50b198865.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
